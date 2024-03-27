495aeb3732e5d431541b40b2a126c454e5d1cd39
Patrick Lima <patrickgdlima@gmail.com>
2024-03-27 11:33:55 -0300

    fix: rm backup sql and fix form devtool

--- packages/dashboard/src/pages/transactions-form/components/expense/transaction-expense-form.tsx
+++ packages/dashboard/src/pages/transactions-form/components/expense/transaction-expense-form.tsx
@@ -1,4 +1,3 @@
-import { DevTool } from "@hookform/devtools"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { addDays } from "date-fns"
 import { CalendarIcon } from "lucide-react"
@@ -64,144 +63,138 @@ const TransactionExpenseForm = ({ onSubmit, transactionToUpdate }: TransactionEx
   }, [transactionToUpdate])
 
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} id="transaction-form">
-          <div className="space-y-8 py-2 pb-4">
-            <div className="flex space-x-4 items-end justify-center w-full">
-              <FormField
-                name="amount"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel htmlFor="input-amount">Valor do pagamento</FormLabel>
-                    <FormControl>
-                      <InputCurrency
-                        id="input-amount"
-                        name="input-amount"
-                        placeholder="R$ 00,00"
-                        defaultValue={field.value}
-                        onCustomChange={field.onChange}
-                        autoFocus
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} id="transaction-form">
+        <div className="space-y-8 py-2 pb-4">
+          <div className="flex space-x-4 items-end justify-center w-full">
             <FormField
+              name="amount"
               control={form.control}
-              name="is_done"
               render={({ field }) => (
-                <FormItem className="flex items-center space-y-0 space-x-4">
-                  <FormLabel className="text-base">Já foi pago?</FormLabel>
+                <FormItem>
+                  <FormLabel htmlFor="input-amount">Valor do pagamento</FormLabel>
                   <FormControl>
-                    <Switch checked={field.value} onCheckedChange={field.onChange} />
+                    <InputCurrency
+                      id="input-amount"
+                      name="input-amount"
+                      placeholder="R$ 00,00"
+                      defaultValue={field.value}
+                      onCustomChange={field.onChange}
+                      autoFocus
+                    />
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
+          </div>
 
-            <div className="flex items-end w-auto">
+          <FormField
+            control={form.control}
+            name="is_done"
+            render={({ field }) => (
+              <FormItem className="flex items-center space-y-0 space-x-4">
+                <FormLabel className="text-base">Já foi pago?</FormLabel>
+                <FormControl>
+                  <Switch checked={field.value} onCheckedChange={field.onChange} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <div className="flex items-end w-auto">
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Data do Pagamento</FormLabel>
+
+                  <div className="flex space-x-1">
+                    <ToggleGroup variant="outline" type="single" onValueChange={handlePresetDate}>
+                      <ToggleGroupItem value="0">Hoje</ToggleGroupItem>
+                      <ToggleGroupItem value="1">Amanhã</ToggleGroupItem>
+                    </ToggleGroup>
+
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-[240px] pl-3 text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? formatDate(field.value, "PPP") : <span>Escolha uma data</span>}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="w-2/4">
+            <FormField
+              name="description"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Descrição</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Breve descrição da transação..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Tabs defaultValue="data" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="data">Dados do Lançamento</TabsTrigger>
+              <TabsTrigger value="notes">Anotações</TabsTrigger>
+
+              <TabsTrigger disabled value="recurrence">
+                Recorrência (em breve)
+              </TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="data">
+              <TransactionDataForm form={form} categories={categories} clientsOrSuppliers={clientsOrSuppliers} />
+            </TabsContent>
+
+            <TabsContent value="notes">
               <FormField
                 control={form.control}
-                name="date"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Data do Pagamento</FormLabel>
-
-                    <div className="flex space-x-1">
-                      <ToggleGroup variant="outline" type="single" onValueChange={handlePresetDate}>
-                        <ToggleGroupItem value="0">Hoje</ToggleGroupItem>
-                        <ToggleGroupItem value="1">Amanhã</ToggleGroupItem>
-                      </ToggleGroup>
-
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              variant={"outline"}
-                              className={cn(
-                                "w-[240px] pl-3 text-left font-normal",
-                                !field.value && "text-muted-foreground"
-                              )}
-                            >
-                              {field.value ? formatDate(field.value, "PPP") : <span>Escolha uma data</span>}
-                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0" align="start">
-                          <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
-                        </PopoverContent>
-                      </Popover>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="w-2/4">
-              <FormField
-                name="description"
-                control={form.control}
+                name="notes"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Descrição</FormLabel>
                     <FormControl>
-                      <Input placeholder="Breve descrição da transação..." {...field} />
+                      <Textarea className="resize-none" {...field} value={field.value ? field.value : ""} />
                     </FormControl>
+                    <FormDescription>Insira qualquer observação que achar relevante para a transação.</FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-            </div>
+            </TabsContent>
 
-            <Tabs defaultValue="data" className="space-y-4">
-              <TabsList>
-                <TabsTrigger value="data">Dados do Lançamento</TabsTrigger>
-                <TabsTrigger value="notes">Anotações</TabsTrigger>
-
-                <TabsTrigger disabled value="recurrence">
-                  Recorrência (em breve)
-                </TabsTrigger>
-              </TabsList>
-
-              <TabsContent value="data">
-                <TransactionDataForm form={form} categories={categories} clientsOrSuppliers={clientsOrSuppliers} />
-              </TabsContent>
-
-              <TabsContent value="notes">
-                <FormField
-                  control={form.control}
-                  name="notes"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Textarea className="resize-none" {...field} value={field.value ? field.value : ""} />
-                      </FormControl>
-                      <FormDescription>
-                        Insira qualquer observação que achar relevante para a transação.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </TabsContent>
-
-              <TabsContent value="recurrence">
-                <TransactionRecurrenceForm form={form} />
-              </TabsContent>
-            </Tabs>
-          </div>
-        </form>
-      </Form>
-
-      <DevTool control={form.control} />
-    </>
+            <TabsContent value="recurrence">
+              <TransactionRecurrenceForm form={form} />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </form>
+    </Form>
   )
 }
 
--- packages/dashboard/src/pages/transactions-form/components/income/transaction-income-form.tsx
+++ packages/dashboard/src/pages/transactions-form/components/income/transaction-income-form.tsx
@@ -1,4 +1,3 @@
-import { DevTool } from "@hookform/devtools"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { addDays } from "date-fns"
 import { CalendarIcon } from "lucide-react"
@@ -59,26 +58,95 @@ const TransactionIncomeForm = ({ onSubmit, transactionToUpdate }: TransactionInc
   }, [transactionToUpdate])
 
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} id="transaction-form">
-          <div className="space-y-8 py-2 pb-4">
-            <div className="flex space-x-4 items-end justify-center w-full">
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} id="transaction-form">
+        <div className="space-y-8 py-2 pb-4">
+          <div className="flex space-x-4 items-end justify-center w-full">
+            <FormField
+              name="amount"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="input-amount">Valor do recebimento</FormLabel>
+                  <FormControl>
+                    <InputCurrency
+                      id="input-amount"
+                      name="input-amount"
+                      placeholder="R$ 00,00"
+                      defaultValue={field.value}
+                      onCustomChange={field.onChange}
+                      autoFocus
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="is_done"
+            render={({ field }) => (
+              <FormItem className="flex items-center space-y-0 space-x-4">
+                <FormLabel className="text-base">Já foi recebido?</FormLabel>
+                <FormControl>
+                  <Switch checked={field.value} onCheckedChange={field.onChange} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <div className="flex items-end w-auto">
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Data do Recebimento</FormLabel>
+
+                  <div className="flex space-x-1">
+                    <ToggleGroup variant="outline" type="single" onValueChange={handlePresetDate}>
+                      <ToggleGroupItem value="0">Hoje</ToggleGroupItem>
+                      <ToggleGroupItem value="1">Amanhã</ToggleGroupItem>
+                    </ToggleGroup>
+
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-[240px] pl-3 text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? formatDate(field.value, "PPP") : <span>Escolha uma data</span>}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="flex w-full space-x-1">
+            <div className="w-2/4">
               <FormField
-                name="amount"
+                name="description"
                 control={form.control}
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel htmlFor="input-amount">Valor do recebimento</FormLabel>
+                    <FormLabel>Descrição</FormLabel>
                     <FormControl>
-                      <InputCurrency
-                        id="input-amount"
-                        name="input-amount"
-                        placeholder="R$ 00,00"
-                        defaultValue={field.value}
-                        onCustomChange={field.onChange}
-                        autoFocus
-                      />
+                      <Input placeholder="Breve descrição da transação..." {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -86,112 +154,39 @@ const TransactionIncomeForm = ({ onSubmit, transactionToUpdate }: TransactionInc
               />
             </div>
 
-            <FormField
-              control={form.control}
-              name="is_done"
-              render={({ field }) => (
-                <FormItem className="flex items-center space-y-0 space-x-4">
-                  <FormLabel className="text-base">Já foi recebido?</FormLabel>
-                  <FormControl>
-                    <Switch checked={field.value} onCheckedChange={field.onChange} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-
-            <div className="flex items-end w-auto">
+            <div className="w-2/4">
               <FormField
                 control={form.control}
-                name="date"
+                name="account_id"
                 render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Data do Recebimento</FormLabel>
-
-                    <div className="flex space-x-1">
-                      <ToggleGroup variant="outline" type="single" onValueChange={handlePresetDate}>
-                        <ToggleGroupItem value="0">Hoje</ToggleGroupItem>
-                        <ToggleGroupItem value="1">Amanhã</ToggleGroupItem>
-                      </ToggleGroup>
-
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              variant={"outline"}
-                              className={cn(
-                                "w-[240px] pl-3 text-left font-normal",
-                                !field.value && "text-muted-foreground"
-                              )}
-                            >
-                              {field.value ? formatDate(field.value, "PPP") : <span>Escolha uma data</span>}
-                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0" align="start">
-                          <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
-                        </PopoverContent>
-                      </Popover>
-                    </div>
+                  <FormItem>
+                    <FormLabel>Conta</FormLabel>
+                    <Select onValueChange={field.onChange} value={field.value}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione uma conta" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {accounts?.length > 0 &&
+                          accounts.map((account) => {
+                            return (
+                              <SelectItem key={account.id} value={account.id}>
+                                {account.name}
+                              </SelectItem>
+                            )
+                          })}
+                      </SelectContent>
+                    </Select>
                     <FormMessage />
                   </FormItem>
                 )}
               />
             </div>
-
-            <div className="flex w-full space-x-1">
-              <div className="w-2/4">
-                <FormField
-                  name="description"
-                  control={form.control}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Descrição</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Breve descrição da transação..." {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <div className="w-2/4">
-                <FormField
-                  control={form.control}
-                  name="account_id"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Conta</FormLabel>
-                      <Select onValueChange={field.onChange} value={field.value}>
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Selecione uma conta" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {accounts?.length > 0 &&
-                            accounts.map((account) => {
-                              return (
-                                <SelectItem key={account.id} value={account.id}>
-                                  {account.name}
-                                </SelectItem>
-                              )
-                            })}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
           </div>
-        </form>
-      </Form>
-
-      <DevTool control={form.control} />
-    </>
+        </div>
+      </form>
+    </Form>
   )
 }
 
