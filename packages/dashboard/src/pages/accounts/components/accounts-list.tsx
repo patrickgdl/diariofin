4345b6cb3c28a0e6318589a5119105bcedaf935f
Patrick Lima <patrickgdlima@gmail.com>
2024-03-27 14:42:56 -0300

    feat: accounts empty state

--- packages/dashboard/src/pages/accounts/components/accounts-list.tsx
+++ packages/dashboard/src/pages/accounts/components/accounts-list.tsx
@@ -26,30 +26,36 @@ export function AccountsList({ items, onSelect }: AccountsListProps) {
   return (
     <ScrollArea className="h-screen">
       <div className="flex flex-col gap-4 p-4 pt-0">
-        {items.map((item) => (
-          <Card
-            key={item.id}
-            onClick={() => onSelect(item)}
-            className="group flex items-center justify-between p-3 hover:cursor-pointer"
-          >
-            <div className="flex grow flex-col">
-              <CardTitle className="text-md font-bold">
-                <div className="flex items-center space-x-4">
-                  <Avatar className="h-5 w-5">
-                    <AvatarImage src={`https://avatar.vercel.sh/${item.id}.png`} alt={item.name} />
-                    <AvatarFallback>{getAcronym(item.name)}</AvatarFallback>
-                  </Avatar>
+        {items.length === 0 ? (
+          <div className="text-center text-gray-400">Sem contas encontradas</div>
+        ) : (
+          <>
+            {items.map((item) => (
+              <Card
+                key={item.id}
+                onClick={() => onSelect(item)}
+                className="group flex items-center justify-between p-3 hover:cursor-pointer"
+              >
+                <div className="flex grow flex-col">
+                  <CardTitle className="text-md font-bold">
+                    <div className="flex items-center space-x-4">
+                      <Avatar className="h-5 w-5">
+                        <AvatarImage src={`https://avatar.vercel.sh/${item.id}.png`} alt={item.name} />
+                        <AvatarFallback>{getAcronym(item.name)}</AvatarFallback>
+                      </Avatar>
 
-                  <span>{item.name}</span>
+                      <span>{item.name}</span>
+                    </div>
+                  </CardTitle>
                 </div>
-              </CardTitle>
-            </div>
-            <div className="flex items-center space-x-4">
-              <p className="flex-1 truncate text-sm font-medium">{item.pix}</p>
-              <ChevronRight className="h-5 w-5 text-gray-400" />
-            </div>
-          </Card>
-        ))}
+                <div className="flex items-center space-x-4">
+                  <p className="flex-1 truncate text-sm font-medium">{item.pix}</p>
+                  <ChevronRight className="h-5 w-5 text-gray-400" />
+                </div>
+              </Card>
+            ))}
+          </>
+        )}
       </div>
     </ScrollArea>
   )
--- packages/dashboard/src/pages/transactions/components/columns.tsx
+++ packages/dashboard/src/pages/transactions/components/columns.tsx
@@ -1,93 +1,96 @@
 import { CheckCircledIcon } from "@radix-ui/react-icons"
 import { ColumnDef } from "@tanstack/react-table"
 import { CircleIcon } from "lucide-react"
+import CategoryBadge from "~/components/category-badge"
 import { TransactionsQuery } from "~/queries/get-transactions"
 import { Avatar, AvatarFallback, AvatarImage } from "~/ui/avatar"
 import { Checkbox } from "~/ui/checkbox"
+import { TableCell } from "~/ui/table"
 import { cn } from "~/utils/cn"
 import formatCurrency from "~/utils/format-currency"
 import { getAcronym } from "~/utils/get-acronym"
 
-import { DataTableRowActions } from "./transactions-row-actions"
 import { TRANSACTION_TYPE } from "../constants"
-import CategoryBadge from "~/components/category-badge"
+import { DataTableRowActions } from "./transactions-row-actions"
 
 export const columns: ColumnDef<TransactionsQuery[0]>[] = [
   {
     id: "select",
     cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        aria-label="Selecionar linha"
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-      />
+      <TableCell>
+        <Checkbox
+          checked={row.getIsSelected()}
+          aria-label="Selecionar linha"
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+        />
+      </TableCell>
     ),
-    enableSorting: false,
-    enableHiding: false,
   },
   {
     accessorKey: "description",
     cell: ({ row }) => (
-      // @ts-ignore
-      <div className={cn("capitalize", row.original.transactions_instance.is_cancelled && "line-through")}>
-        {row.getValue("description")}
-      </div>
+      <TableCell className="w-1/5">
+        {/* @ts-ignore */}
+        <div className={cn("capitalize", row.original.transactions_instance.is_cancelled && "line-through")}>
+          {row.getValue("description")}
+        </div>
+      </TableCell>
     ),
-    enableResizing: false,
   },
   {
     accessorKey: "is_done",
     cell: ({ row }) => {
       return (
-        <div className="flex w-[150px] items-center">
-          {/* @ts-ignore */}
-          {row.original.transactions_instance?.is_done ? (
-            <CheckCircledIcon className="mr-2 h-4 w-4 text-muted-foreground" />
-          ) : (
-            <CircleIcon className="mr-2 h-4 w-4 text-muted-foreground" />
-          )}
-          {/* @ts-ignore */}
-          <span>{row.original.transactions_instance?.is_done ? "Confirmado" : "Não confirmado"}</span>
-        </div>
+        <TableCell className="w-1/5">
+          <div className="flex w-[150px] items-center">
+            {/* @ts-ignore */}
+            {row.original.transactions_instance?.is_done ? (
+              <CheckCircledIcon className="mr-2 h-4 w-4 text-muted-foreground" />
+            ) : (
+              <CircleIcon className="mr-2 h-4 w-4 text-muted-foreground" />
+            )}
+            {/* @ts-ignore */}
+            <span>{row.original.transactions_instance?.is_done ? "Confirmado" : "Á confirmar"}</span>
+          </div>
+        </TableCell>
       )
     },
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id))
     },
-    enableResizing: false,
   },
   {
     accessorKey: "transaction_categories",
     cell: ({ row }) => {
       return (
-        <>
+        <TableCell className="w-1/5">
           {row.original.transaction_types?.id === TRANSACTION_TYPE.EXPENSE ? (
             <CategoryBadge category={row.original.transaction_categories!} />
           ) : null}
-        </>
+        </TableCell>
       )
     },
-    enableResizing: false,
   },
   {
     accessorKey: "account",
     cell: ({ row }) => (
-      <div className="flex items-center space-x-1">
-        <Avatar className="h-5 w-5">
-          <AvatarImage
-            src={`https://avatar.vercel.sh/${row.original.account?.id}.png`}
-            alt={row.original.account?.name || ""}
-          />
-          <AvatarFallback>{getAcronym(row.original.account?.name || "")}</AvatarFallback>
-        </Avatar>
+      <TableCell className="w-1/5">
+        <div className="flex items-center space-x-1">
+          <Avatar className="h-5 w-5">
+            <AvatarImage
+              src={`https://avatar.vercel.sh/${row.original.account?.id}.png`}
+              alt={row.original.account?.name || ""}
+            />
+            <AvatarFallback>{getAcronym(row.original.account?.name || "")}</AvatarFallback>
+          </Avatar>
 
-        <span>{row.original.account?.name}</span>
-      </div>
+          <span>{row.original.account?.name}</span>
+        </div>
+      </TableCell>
     ),
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id))
     },
-    enableResizing: false,
   },
   {
     accessorKey: "amount",
@@ -95,7 +98,11 @@ export const columns: ColumnDef<TransactionsQuery[0]>[] = [
       const amount = parseFloat(row.getValue("amount"))
 
       return (
-        <div className={cn("text-right font-medium", amount < 0 ? "" : "text-green-500")}>{formatCurrency(amount)}</div>
+        <TableCell className="w-1/5">
+          <div className={cn("text-right font-medium", amount < 0 ? "" : "text-green-500")}>
+            {formatCurrency(amount)}
+          </div>
+        </TableCell>
       )
     },
   },
@@ -103,10 +110,12 @@ export const columns: ColumnDef<TransactionsQuery[0]>[] = [
     id: "actions",
     cell: ({ row }) => {
       // @ts-ignore
-      return row.original.transactions_instance.is_cancelled ||
-        // @ts-ignore
-        row.original.transactions_instance.is_refunded ? null : (
-        <DataTableRowActions row={row} />
+      return row.original.transactions_instance.is_cancelled || row.original.transactions_instance.is_refunded ? (
+        <TableCell />
+      ) : (
+        <TableCell>
+          <DataTableRowActions row={row} />
+        </TableCell>
       )
     },
   },
