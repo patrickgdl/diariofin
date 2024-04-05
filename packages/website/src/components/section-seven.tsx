5b9df60dd96e0e9190f8aa33cc89b843caac0cd1
Patrick Lima <patrickgdlima@gmail.com>
2024-04-05 12:19:28 -0300

    fix: typo

--- packages/website/src/components/section-seven.tsx
+++ packages/website/src/components/section-seven.tsx
@@ -4,7 +4,7 @@ export function SectionSeven() {
   return (
     <TextGenerateEffect
       className="pt-28 pb-40 text-4xl	md:text-6xl max-w-[1370px] container md:leading-[85px] mb-12"
-      words="Coloque as partes chatas de administrar uma empresa no piloto automático. A FluxoZen ajuda você a agilizar seus procedimentos de final de mês, reduzir o trabalho manual e automatizando suas finanças."
+      words="Coloque as partes chatas de administrar uma empresa no piloto automático. A FluxoZen ajuda você a agilizar seus procedimentos de final de mês, reduzir o trabalho manual e automatizar suas finanças."
     />
   );
 }
